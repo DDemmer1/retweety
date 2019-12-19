959842fca64056444683c67ee38a6fde342af126
Dennis Demmer <Dennis.Demmer@web.de>
2019-12-19 17:32:06 +0100

    Initial commit

---
+++ retweety.py
@@ -0,0 +1,30 @@
+import tweepy
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
+
+
+def retweet():
+    search = ("myhashtag")
+
+    numberofTweets = 5
+    for tweet in tweepy.Cursor(api.search, search).items(numberofTweets):
+        try:
+            tweet.retweet()
+            print("Tweet Retweeted")
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+
+
+retweet()
